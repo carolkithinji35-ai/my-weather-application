1e2c18e0d064047d1c41f4b27fb4b113f82f035f
carolkithinji35-ai <carolkithinji35@gmail.com>
2025-12-02 02:33:50 +0300

    Fetch weather API

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
+function getTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature-element");
+  let Temperature = Math.round(response.data.temperature.current);
+  let city = document.querySelector("#city");
+  city = response.data.city;
+  temperatureElement.innerHTML = Temperature;
+
+  
+}
+
 function searchForCity(event) {
   event.preventDefault();
   let cityInput = document.querySelector("#search-input");
   let h1 = document.querySelector("h1");
   h1.innerHTML = cityInput.value;
+
+  let city = cityInput.value;
+  let apiKey = "568925f93ofcc31043b0b8154ebt149a";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(getTemperature);
 }
 
 let form = document.querySelector("form");
 form.addEventListener("submit", searchForCity);
-
-
-
-function getTemperature(response) {
-  let currentTemperature = response.data.temperature.current;
-  console.log(currentTemperature)
-}
-
-let city = "Mombasa";
-let apiKey = "568925f93ofcc31043b0b8154ebt149a";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
-
-axios.get(apiUrl).then(getTemperature);
